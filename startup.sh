c65833300a794e201e83e8f3f1fc5b0aae5fca9c
cameron <cuckmeron@gmail.com>
2025-03-12 15:59:32 -0400

    .

--- startup.sh
+++ startup.sh
@@ -4,6 +4,9 @@ while true; do
     git fetch origin
     git reset --hard origin/master
 
+    psql -U postgres -d potpissers -f semicolon-separated-postgres.sql
+    -u postgres psql -d potpissers
+
     echo Server restarting...
     echo Press CTRL + C to stop.
 done
