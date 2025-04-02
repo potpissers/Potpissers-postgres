be101f1166218218c4f78cb9e91453fa6f092669
cameron <cuckmeron@gmail.com>
2025-04-02 01:34:54 -0400

    .

--- main.sql
+++ main.sql
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS user_referrals
 CREATE OR REPLACE FUNCTION get_user_referral_exists(user_uuid UUID, ip TEXT, key TEXT)
     RETURNS BOOLEAN AS
 $$
-WITH cte AS (SELECT pgp_sym_decrypt(pgcrypto_aes_referrer, key, 'cipher-algo=aes128, s2k-mode=0') AS referrer
+WITH cte AS (SELECT pgp_sym_decrypt(pgcrypto_aes_referrer, key, 'cipher-algo=aes128, s2k-mode=0, s2k-digest-algo=sha1, compress-algo=0') AS referrer
              FROM ip_referrals
-             WHERE pgcrypto_aes_ip = pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0')),
+             WHERE pgcrypto_aes_ip = pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0, s2k-digest-algo=sha1, compress-algo=0')),
      _ AS (INSERT INTO user_referrals (user_uuid, referrer) SELECT get_user_referral_exists.user_uuid, referrer
                                                             FROM cte
                                                             WHERE EXISTS(SELECT * FROM cte))
@@ -30,8 +30,8 @@ CREATE OR REPLACE PROCEDURE insert_user_referral(ip TEXT, key TEXT, referrer TEX
 AS
 $$
 WITH cte
-         AS (INSERT INTO ip_referrals (pgcrypto_aes_ip, pgcrypto_aes_referrer) VALUES (pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0'),
-                                                                                       pgp_sym_encrypt(referrer, key, 'cipher-algo=aes128, s2k-mode=0')) ON CONFLICT DO NOTHING RETURNING *)
+         AS (INSERT INTO ip_referrals (pgcrypto_aes_ip, pgcrypto_aes_referrer) VALUES (pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0, s2k-digest-algo=sha1, compress-algo=0'),
+                                                                                       pgp_sym_encrypt(referrer, key, 'cipher-algo=aes128, s2k-mode=0, s2k-digest-algo=sha1, compress-algo=0')) ON CONFLICT DO NOTHING RETURNING *)
 INSERT
 INTO user_referrals (user_uuid, referrer)
 SELECT insert_user_referral.user_uuid, insert_user_referral.referrer
@@ -47,7 +47,7 @@ WITH cte AS (INSERT INTO user_referrals (user_uuid, timestamp)
     VALUES (handle_upsert_user_referral.user_uuid, handle_upsert_user_referral.timestamp)
     ON CONFLICT (user_uuid) DO UPDATE SET timestamp = EXCLUDED.timestamp RETURNING *),
      _
-         AS (INSERT INTO ip_referrals (pgcrypto_aes_ip) VALUES (pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0')) ON CONFLICT DO NOTHING)
+         AS (INSERT INTO ip_referrals (pgcrypto_aes_ip) VALUES (pgp_sym_encrypt(ip, key, 'cipher-algo=aes128, s2k-mode=0, s2k-digest-algo=sha1, compress-algo=0')) ON CONFLICT DO NOTHING)
 SELECT pg_notify('referrals',
                  (SELECT json_build_object('player_uuid', cte.user_uuid, 'referrer', cte.referrer, 'timestamp',
                                            cte.timestamp, 'row_number',
